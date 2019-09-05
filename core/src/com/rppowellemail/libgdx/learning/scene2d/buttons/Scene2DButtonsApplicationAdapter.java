971e2d8c7b512887b580007a04ffd0212a3bfed1
Rob Powell <rppowellemail@gmail.com>
2019-09-05 15:36:16 -0700

    Adding CheckBox1

--- core/src/com/rppowellemail/libgdx/learning/scene2d/buttons/Scene2DButtonsApplicationAdapter.java
+++ core/src/com/rppowellemail/libgdx/learning/scene2d/buttons/Scene2DButtonsApplicationAdapter.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
@@ -16,6 +17,7 @@ public class Scene2DButtonsApplicationAdapter extends ApplicationAdapter {
 
 	private Button button1;
 	private Button button2;
+	private CheckBox checkBox1;
 
 	@Override
 	public void create () {
@@ -32,6 +34,7 @@ public class Scene2DButtonsApplicationAdapter extends ApplicationAdapter {
 			public void clicked(InputEvent event, float x, float y) {
 			    Gdx.app.log("Scene2DButtonsApplicationAdapter", "button2.toggle()");
 				button2.toggle();
+				checkBox1.toggle();
 			}
 		});
 
@@ -47,6 +50,12 @@ public class Scene2DButtonsApplicationAdapter extends ApplicationAdapter {
 		});
 
 		stage.addActor(button2);
+
+		checkBox1 = new CheckBox("1", skin);
+		checkBox1.setSize(32, 32);
+		checkBox1.setPosition(160, 32);
+
+		stage.addActor(checkBox1);
 
 		Gdx.input.setInputProcessor(stage);
 	}
